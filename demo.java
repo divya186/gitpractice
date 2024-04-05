9f9e46eab80d3254be39445d5de204628de1f760
divya186 <challadivya2000@gmail.com>
2024-04-05 22:13:24 +0530

    third

--- demo.java
+++ demo.java
@@ -1,2 +1,2 @@
 #once commit is done we cannot restore the changes to the file
-
+#why clone is used means that is repository where we have to do our work and we have to go our working dir to add any files or fiolders
